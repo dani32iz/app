9e81e09320ef80c7609b008c1ec60c3c28ee7b51
Bazhenov Daniil <dbazhenov@simtechdev.com>
2016-11-20 16:28:58 +0400

    Init

---
+++ src/Connect.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Simtechdev\App;
+
+class Connect
+{
+    /**
+     * Create a new Instance
+     */
+    public function __construct()
+    {
+        // constructor body
+    }
+
+    /**
+     * Friendly welcome
+     *
+     * @param string $phrase Phrase to return
+     *
+     * @return string Returns the phrase passed in
+     */
+    public function echoPhrase($phrase)
+    {
+        return $phrase;
+    }
+}
